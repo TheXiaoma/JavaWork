3c3d9a50572c6cf90ffb7df4ab336ad8572e7391
GXStudio <xiaoma0668@outlook.com>
2023-06-25 23:04:51 +0800

    changed.

--- src/com/xiaoma/java/classwork/methodwork/ArrayCopier.java
+++ src/com/xiaoma/java/classwork/methodwork/ArrayCopier.java
@@ -3,7 +3,7 @@ package com.xiaoma.java.classwork.methodwork;
 public class ArrayCopier {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
-        printArray(copyArray(arr, 1, 3));
+        ArrayPrinter.printArray(copyArray(arr, 1, 3));
     }
 
     public static int[] copyArray(int[] inputArray, int startIndex, int endIndex) {
@@ -12,17 +12,5 @@ public class ArrayCopier {
             newArray[i - startIndex] = inputArray[i];
         }
         return newArray;
-    }
-
-    public static void printArray(int[] inputArray) {
-        for (int i = 0; i < inputArray.length; i++) {
-            if (i == 0) {
-                System.out.print("[ " + inputArray[i] + " , ");
-            } else if (i == inputArray.length - 1) {
-                System.out.println(inputArray[i] + " ]");
-            } else {
-                System.out.print(inputArray[i] + " , ");
-            }
-        }
     }
 }
