622a4bf78cf5c6dbed586718f742dfadd3825d51
GXStudio <xiaoma0668@outlook.com>
2023-06-25 21:59:08 +0800

    add a classwork

---
+++ src/com/xiaoma/java/classwork/method/SumCalculator.java
@@ -0,0 +1,11 @@
+package com.xiaoma.java.classwork.method;
+
+public class SumCalculator {
+    public static void main(String[] args) {
+        System.out.println(calculateSum(1, 2, 3));
+    }
+
+    public static int calculateSum(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
+}
